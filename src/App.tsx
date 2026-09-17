f8ba87629cbf1d7660553ae6b4faba0b5070188e
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2026-09-17 14:28:20 +0200

    chore: scaffold Vite React TS project, remove demo content, add CSS Modules base

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <main className={styles.app}>
+      <h1>Norwegian Weather</h1>
+    </main>
+  );
+}
+
+export default App
