2035ac712f6d1b2cfeb7b4d0e07e22f6db820d36
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2026-09-18 15:20:39 +0200

    feat: add component skeleton composing CitySelector and WeatherDisplay

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,12 @@
+import WeatherIcon from "./WeatherIcon";
+
+function WeatherCard() {
+  return (
+    <div>
+      <h2>Weather Card Component</h2>
+      <WeatherIcon />
+    </div>
+  );
+}
+
+export default WeatherCard;
